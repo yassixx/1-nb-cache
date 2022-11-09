0a1955ead8b5e56e6add22129063b05c3551674a
intad <intad@LAPTOP-TU7FB3NG>
2022-11-09 12:00:46 +0100

    Ajoutez des fichiers projet.

---
+++ 1 nb cache/Program.cs
@@ -0,0 +1,59 @@
+﻿/**
+ * Jeu du nombre caché
+ * Fakiri Yassine
+ * 07/11/22
+ */
+
+using System;
+namespace NombreCache
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int nbAChercher, val, i = 0;
+            bool correct = false;
+            while(correct != true)
+            {
+                try
+                {
+                    Console.WriteLine("Entrez le nombre à chercher : ");
+                    nbAChercher = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Erreur de saisie : saisissez un nombre entier");
+
+                }
+            }
+            correct = false;
+            Console.Clear();
+            do
+            {
+                while (correct != true)
+                {
+                    try
+                    {
+                        Console.Write("Entrez un essai : ");
+                        val = int.Parse(Console.ReadLine());
+                        correct = true;
+                    }
+                    catch {
+                        Console.WriteLine("Erreur de saisie : saisissez un nombre entier");
+                    }
+                }
+               
+                if (val > nbAChercher)
+                {
+                    Console.WriteLine("--> trop grand !");
+                }
+                else
+                {
+                    Console.WriteLine("--> trop petit !");
+                }
+                i++;
+            } while (val != nbAChercher);
+            Console.WriteLine("bravo vous avez trouve en " + i + " fois");
+        }
+    }
+}
